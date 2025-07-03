8664ce3df84781bb88ec8dd227326d8d26ff8bc9
amink7 <amin_lan@hotmail.es>
2025-07-03 03:42:51 +0200

    limpio repo

--- backend/auth-service/src/main/java/com/cybersecurity/auth_service/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cybersecurity.auth_service.repository;
-
-import com.cybersecurity.auth_service.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, String> {
-}
